c9ed045ee39a4e2e10dcb65bbafe3d8f1e74c584
Daniil Zakharov <danilazakharov2003@gmail.com>
2025-01-26 13:38:57 +0300

    change test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -34,15 +34,8 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+79154230549");
         $("[data-test-id='agreement']").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
-
-        //$(Selectors.withText("Встреча успешно забронирована на"))
-        //        .should(Condition.visible, Duration.ofSeconds(15));
-        //$(Selectors.withText(date)).should(Condition.visible);
-
-        //$(By.cssSelector("Встреча успешно забронирована на")).should(Condition.visible)
-        //        .shouldHave(Condition.exactText(date),Duration.ofSeconds(15));
-
-        $(Selectors.withText(date))
-                .should(Condition.visible, Duration.ofSeconds(15));
+        $(By.cssSelector(".notification__content"))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
+                .should(Condition.visible);
     }
 }
